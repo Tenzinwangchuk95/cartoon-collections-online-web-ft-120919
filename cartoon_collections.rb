23d53ba26ed0326158880dbe97c7c083e0dbc7ee
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-12 22:11:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   return array.detect do |cheese|
-    cheese == (cheese_types.split)
+    cheese.include? 
   end
 end
